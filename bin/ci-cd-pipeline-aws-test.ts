547d8d6c809b34e89edc13e67d73b7126836c029
LozadaZ <47724135+LozadaZ@users.noreply.github.com>
2026-01-15 15:41:42 -0600

    commit

--- bin/ci-cd-pipeline-aws-test.ts
+++ bin/ci-cd-pipeline-aws-test.ts
@@ -5,7 +5,7 @@ import { CiCdPipelineAwsTestStack } from '../lib/ci-cd-pipeline-aws-test-stack';
 const app = new cdk.App();
 new CiCdPipelineAwsTestStack(app, 'CiCdPipelineAwsTestStack', {
    env: {
-    account: '755314965794',
+    account: '534313115007',
     region: 'us-east-1',
   }
 });

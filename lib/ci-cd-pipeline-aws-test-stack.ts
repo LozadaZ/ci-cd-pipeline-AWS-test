ffc68e1e2aab0b312d949a31487916c9047b9038
LozadaZ <47724135+LozadaZ@users.noreply.github.com>
2026-01-15 16:32:09 -0600

    initializing lambda

--- lib/ci-cd-pipeline-aws-test-stack.ts
+++ lib/ci-cd-pipeline-aws-test-stack.ts
@@ -2,13 +2,13 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep, Step } from 'aws-cdk-lib/pipelines';
 import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
-//import { MyPipelineAppStage } from './stage';
+import { MyPipelineAppStage } from './stage';
 
 export class CiCdPipelineAwsTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-   new CodePipeline(this, 'Pipeline', {
+   const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'TestPipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('LozadaZ/ci-cd-pipeline-AWS-test', 'main'), //Remember to change 
@@ -17,5 +17,20 @@ export class CiCdPipelineAwsTestStack extends cdk.Stack {
                    'npx cdk synth']
       })
     });
+
+
+    const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
+      env: { account: "755314965794", region: "us-east-1" }
+    }));
+
+
+    testingStage.addPre(new ShellStep("Run Unit Tests", { commands: ['npm install', 'npm test'] }));
+    testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
+
+    const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod", {
+      env: { account: "534313115007", region: "us-east-1" }
+    }));
   }
 }
+
+
